509f4a1844e7b370fc7e8810216a4a0544f63ae3
KunonaEthan78 <kethanmalco@gmail.com>
2025-07-17 13:16:37 +0300

    Add files via upload

---
+++ server/Java-server/src/main/java/com/vendor/model/VendorRequest.java
@@ -0,0 +1,31 @@
+package com.vendor.model;
+
+public class VendorRequest {
+    private Long vendorId;
+    private String pdfUrl;
+    private String callbackUrl;
+
+    public Long getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(Long vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getPdfUrl() {
+        return pdfUrl;
+    }
+
+    public void setPdfUrl(String pdfUrl) {
+        this.pdfUrl = pdfUrl;
+    }
+
+    public String getCallbackUrl() {
+        return callbackUrl;
+    }
+
+    public void setCallbackUrl(String callbackUrl) {
+        this.callbackUrl = callbackUrl;
+    }
+}
